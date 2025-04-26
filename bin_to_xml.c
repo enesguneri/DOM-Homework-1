e4aef302920dfd82712122237b0dbb3275849af3
enesguneri <enesarda62@hotmail.com>
2025-04-26 23:47:32 +0300

    generate xml kısmı istenen formata göre düzenlendi.

--- bin_to_xml.c
+++ bin_to_xml.c
@@ -6,18 +6,26 @@
 #include <libxml/tree.h>
 
 typedef struct {
-    char *data;
-    int key;
+    char device_id[10];
+    char timestamp[20];
+    float temperature;
+    float humidity;
+    char status[10];
+    char location[50];
+    char alert_level[20];
+    int battery;
+    char firmware_ver[20];
+    int event_code;
 } Record;
 
 int compare_records(const void *a, const void *b) {
     Record *recordA = (Record *)a;
     Record *recordB = (Record *)b;
     
-    if (strcmp(recordA->order, "ASC") == 0) {
-        return recordA->key - recordB->key;
+    if (strcmp(recordA-> location, "ASC") == 0) {
+        return recordA->device_id - recordB->device_id;
     } else {
-        return recordB->key - recordA->key;
+        return recordB->device_id - recordA->device_id;
     }
 }
 
@@ -74,13 +82,17 @@ int read_binary_file(const char *filename, int keyStart, int keyEnd, int *record
     *records = malloc(sizeof(Record) * (*recordCount));
 
     for (int i = 0; i < *recordCount; i++) {
-        (*records)[i].data = malloc(recordSize);
-        fread((*records)[i].data, 1, recordSize, file);
-
-        (*records)[i].key = 0;
-        for (int j = keyStart; j < keyEnd; j++) {
-            (*records)[i].key = (*records)[i].key * 256 + (unsigned char)(*records)[i].data[j];
-        }
+        fread((*records)[i].alert_level, sizeof((*records)[i].alert_level), 8,file);
+        fread(&(*records)[i].battery, sizeof((*records)[i].battery), 8,file);
+        fread((*records)[i].device_id, sizeof((*records)[i].device_id), 8,file);
+        fread(&(*records)[i].event_code, sizeof((*records)[i].event_code), 8,file);
+        fread((*records)[i].firmware_ver, sizeof((*records)[i].firmware_ver), 8,file);
+        fread(&(*records)[i].humidity, sizeof((*records)[i].humidity), 8,file);
+        fread((*records)[i].location, sizeof((*records)[i].location), 8,file);
+        fread((*records)[i].status, sizeof((*records)[i].status), 8,file);
+        fread(&(*records)[i].temperature, sizeof((*records)[i].temperature), 8,file);
+        fread((*records)[i].timestamp, sizeof((*records)[i].timestamp), 8,file);
+        
     }
 
     fclose(file);
@@ -89,13 +101,43 @@ int read_binary_file(const char *filename, int keyStart, int keyEnd, int *record
 
 void generate_xml(const char *filename, Record *records, int recordCount) {
     xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
-    xmlNodePtr root_element = xmlNewNode(NULL, BAD_CAST "Records");
+    xmlNodePtr root_element = xmlNewNode(NULL, BAD_CAST "smartlogs");
     xmlDocSetRootElement(doc, root_element);
 
     for (int i = 0; i < recordCount; i++) {
-        xmlNodePtr record_element = xmlNewChild(root_element, NULL, BAD_CAST "Record", NULL);
-        xmlNewChild(record_element, NULL, BAD_CAST "Key", BAD_CAST "");
-        xmlNewChild(record_element, NULL, BAD_CAST "Data", BAD_CAST records[i].data);
+        xmlNodePtr entry = xmlNewChild(root_element, NULL, BAD_CAST "entry", NULL);
+
+        xmlNodePtr device = xmlNewChild(entry, NULL, BAD_CAST "device", BAD_CAST "");
+        char tempID[10];
+        snprintf(tempID, sizeof(tempID), "%d", i+1);//converting int to string
+        xmlNewProp(device, BAD_CAST "id", BAD_CAST tempID);//adding attribute to entry node(id is starting from 1)
+
+        xmlNewChild(device, NULL, BAD_CAST "device_id", BAD_CAST records[i].device_id);
+        xmlNewChild(device, NULL, BAD_CAST "location", BAD_CAST records[i].location);
+        xmlNewChild(device, NULL, BAD_CAST "firmware_ver", BAD_CAST records[i].firmware_ver);
+
+        xmlNodePtr metrics = xmlNewChild(entry, NULL, BAD_CAST "metrics", NULL);
+        xmlNewProp(metrics, BAD_CAST "status", BAD_CAST records[i].status);
+        xmlNewProp(metrics, BAD_CAST "alert_level", BAD_CAST records[i].alert_level);
+
+        char strTmprtr[10];//we dont have a function to write float value to xml
+        snprintf(strTmprtr, sizeof(strTmprtr), "%f", records[i].temperature);//converting float to string
+        xmlNewChild(metrics, NULL, BAD_CAST "temperature", BAD_CAST strTmprtr);
+
+        char strHumidity[10];
+        snprintf(strHumidity, sizeof(strHumidity), "%f", records[i].humidity);
+        xmlNewChild(metrics, NULL, BAD_CAST "humidity", BAD_CAST strHumidity);
+
+        char strBattery[10];
+        snprintf(strBattery, sizeof(strBattery), "%d", records[i].battery);
+        xmlNewChild(metrics, NULL, BAD_CAST "battery", BAD_CAST strBattery);
+
+        xmlNewChild(entry, NULL, BAD_CAST "timestamp", BAD_CAST records[i].timestamp);
+
+        char strEventCode[10];
+        snprintf(strEventCode, sizeof(strEventCode), "%d", records[i].event_code);
+        xmlNewChild(entry, NULL, BAD_CAST "event_code", BAD_CAST strEventCode);
+        xmlNewProp(entry, BAD_CAST "", BAD_CAST "");//Attribute olarak big endian değerleri eklenecek
     }
 
     xmlSaveFormatFileEnc(filename, doc, "UTF-8", 1);
@@ -111,19 +153,24 @@ int main() {
         return 1;
     }
 
+    printf("Key Start: %d\n", keyStart);
+    printf("Key End: %d\n", keyEnd);
+    printf("Order: %s\n", order);
+    printf("Data File Name: %s\n", dataFileName);
+
     int recordCount;
     Record *records;
     if (read_binary_file(dataFileName, keyStart, keyEnd, &recordCount, &records)) {
         return 1;
     }
 
-    qsort(records, recordCount, sizeof(Record), compare_records);
+    //qsort(records, recordCount, sizeof(Record), compare_records);
 
-    generate_xml("output.xml", records, recordCount);
+    generate_xml("smartlogs.xml", records, recordCount);
 
-    for (int i = 0; i < recordCount; i++) {
+    /*for (int i = 0; i < recordCount; i++) {
         free(records[i].data);
-    }
+    }*/
     free(records);
 
     printf("XML file generated successfully.\n");
