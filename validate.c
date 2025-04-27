e26aebbc4f214e4669663ad0e39192763e3fdf89
uKmaz <emre1563242a@gmail.com>
2025-04-27 13:27:28 +0200

    XSD Dosyasi eklendi

--- validate.c
+++ validate.c
@@ -7,8 +7,8 @@ int main()
     xmlSchemaPtr schema = NULL;
     xmlSchemaParserCtxtPtr ctxt;
 	
-    char *XMLFileName = "shipto.xml"; // write your xml file here
-    char *XSDFileName = "shipto.xsd"; // write your xsd file here
+    char *XMLFileName = "smartlogs.xml"; // write your xml file here
+    char *XSDFileName = "smartlogs.xsd"; // write your xsd file here
     
     
     xmlLineNumbersDefault(1); //set line numbers, 0> no substitution, 1>substitution
